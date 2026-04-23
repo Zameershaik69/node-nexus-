0890a7c71448a829c5248234571e97b75ac18bad
itzzayeshatech <itzzzsiddique@gmail.com>
2026-04-23 06:41:52 -1200

    Remove accidental nested repo

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
-    res.end("Hello World");
+
+    if (req.url === "/") {
+        res.end("Home Page");
+    }
+    else if (req.url === "/about") {
+        res.end("About Page");
+    }
+    else {
+        res.end("Page Not Found");
+    }
 });
 
 server.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server running on port 3000");
 });
